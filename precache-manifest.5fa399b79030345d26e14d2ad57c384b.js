6e339fe7a498755c05cb2510daf9b06509b5e441
ZhongmianWang <zwang@datanetusa.com>
2020-07-10 17:04:28 -0400

    Add build files

---
+++ precache-manifest.5fa399b79030345d26e14d2ad57c384b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ced8b93a01254ebdb9482690a0f1659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca919b0d23a250c9309d",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ba46a45cc9d924864ee1",
+    "url": "/static/js/2.a240132d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a240132d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca919b0d23a250c9309d",
+    "url": "/static/js/main.8fd16736.chunk.js"
+  },
+  {
+    "revision": "5be29c4be381b0a264ae",
+    "url": "/static/js/runtime-main.9ae09e8f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
