b0cbfd4247666709e690f42776b9dafc98bfb89f
ZhongmianWang <zwang@datanetusa.com>
2020-07-10 17:28:44 -0400

    Updates

---
+++ precache-manifest.5d6a6f9d09dfb78c5b651c58012e7b62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c1c0a7420f8330fc979875d9290fe3",
+    "url": "/ServiceWorkerTester/index.html"
+  },
+  {
+    "revision": "d8129cc6aa0b58aa0fbd",
+    "url": "/ServiceWorkerTester/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ba46a45cc9d924864ee1",
+    "url": "/ServiceWorkerTester/static/js/2.a240132d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ServiceWorkerTester/static/js/2.a240132d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8129cc6aa0b58aa0fbd",
+    "url": "/ServiceWorkerTester/static/js/main.4e8133ba.chunk.js"
+  },
+  {
+    "revision": "b83ab70ee80d22738973",
+    "url": "/ServiceWorkerTester/static/js/runtime-main.46b46d8f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ServiceWorkerTester/static/media/logo.ee7cd8ed.svg"
+  }
+]);
